c905dee4515c414a40efc94a8348cf8c95ecb0a8
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-15 08:54:20 -0700

    5-printf->tr_0

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * mian - Entry point
+ *
+ * This program is to use printf function to print "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
