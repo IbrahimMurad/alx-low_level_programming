cb68ae7f61625fe208d652daab200cae09d8f267
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-15 11:18:12 -0700

    101-quote->tr_2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,8 +11,7 @@
 int main(void)
 {
 	char *err_message = "and that piece of art is useful\""
-	       "	- Dora Korpar, 2015-10-19";
-	
+	       " - Dora Korpar, 2015-10-19";
 	fwrite(err_message, sizeof(char), strlen(err_message), stderr);
 	return (1);
 }
