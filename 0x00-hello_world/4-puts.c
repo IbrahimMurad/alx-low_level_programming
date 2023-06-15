97948806499093a7a5d6f0e4b75041ca27f0686c
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-15 08:46:56 -0700

    4-puts->tr_0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * This program is to use puts function
+ * puts prints "Programming is like building a multilingual puzzle"
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
