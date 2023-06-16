cbcbe3fe83e90fd786d0090ec756152966e72338
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-16 13:21:22 +0000

    $mymessage

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This program prints all the alphabets uppercase and lower case
+ * using putchar function only
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
