4819b89e684d7f18d8025fe1afaf853465b9ffd1
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-16 19:41:04 +0000

    102-print_comb5-tr_0

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This prog prints all possible combinations of two two-digit numbers.
+ * the numbers should range from 00 to 99
+ * the two numbers should be separated by space
+ * the combination of numbers are separated by , followed by a space
+ * 00 01 and 01 00 are considered the same combination of numbers
+ * usuing only putchar function (maximum 5 times)
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = i; k <= '9'; k++)
+			{
+				for (l = j + 1; l <= '9'; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (!(i == '9' && j == '8' && k == '9' && l == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
