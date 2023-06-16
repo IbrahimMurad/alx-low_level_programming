6457e14594eafacf5978dbf51b08e294e2617a89
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-16 12:32:03 +0000

    0-positive_or_negative->tr_0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,6 +8,7 @@
  * This program prints a random integer every time it runs
  * It uses the rand function to generate the number
  * printf function prints the number to teh screen
+ * 
  * Exit: Always (0) (success)
  */
 
