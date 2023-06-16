6f3b0d2cd4615323e382a6299c4ce832d0a74de5
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-16 18:02:02 +0000

    100-print_comb3-tr_0

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This prog prints all possible different combinations of two digits
+ * separated by , followed by a space
+ * the two digits must be different
+ * 01 and 10 are considered the same combination
+ * printing only the smallest combination
+ * usuing only putchar function (maximum 5 times)
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, j;
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (!((i == '8') && (j == '9')))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
