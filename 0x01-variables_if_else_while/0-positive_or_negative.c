efd5c5eada47b2a846d26e0b9a77ebe980f0a081
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-16 12:30:38 +0000

    0-positive_or_negative->tr_0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * This program prints a random integer every time it runs
+ * It uses the rand function to generate the number
+ * printf function prints the number to teh screen
+ * Exit: Always (0) (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero\n", n);
+	}
+	else
+	{
+		printf("%i is negative\n", n);
+	}
+	return (0);
+}
