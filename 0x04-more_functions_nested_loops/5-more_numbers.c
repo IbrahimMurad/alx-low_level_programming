9359952c879f6eb331ccdbb0901d14671dfe5ec1
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-22 22:12:29 +0000

    more functions and nested loops tasks

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,14 +15,13 @@ void more_numbers(void)
 	{
 		int j;
 
-		for (j = '0'; j <= '9'; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j);
-		}
-		for (j = '0'; j <= '4'; j++)
-		{
-			_putchar('1');
-			_putchar(j);
+			if (j > 9)
+			{
+				_putchar('1');
+			}
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
