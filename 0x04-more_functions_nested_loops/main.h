87b370cdd2f9a414a82d9343a30973f174c19852
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-22 19:23:46 +0000

    more functions and nested loops tasks

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+
+/**
+ *  _putchar - writes the character c to stdout
+ *  @c: The character to print
+ *  Return: On success 1.
+ *  On error, -1 is returned, and errno is set appropriately.
+ */
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+int _isupper(int c);
+
+#endif /* MAIN_H */
