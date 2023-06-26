dbc5617497f97fda9e0872b8cdc25bf13a4645bf
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-26 15:34:41 +0000

    Pointers are fun

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+/**
+ * _strcpy - copies a string to a buffer
+ * @dest: the buffer
+ * @src: the string
+ *
+ * Return: a pointer to the buffer @dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
