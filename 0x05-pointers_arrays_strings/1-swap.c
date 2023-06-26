2974b4f6a640e058fff5083a4511590e270d49ad
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-26 11:47:55 +0000

    Pointers are fun

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers.
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *b;
+	*b = *a;
+	*a = temp;
+}
+
