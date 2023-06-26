fdd3c12632e88f837e5d3de37c295a0aa14a1464
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-26 21:17:00 +0000

    Pointers are fun

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,27 +14,21 @@ int _atoi(char *s)
 
 	index = 0;
 	minus = 0;
-
-	/* finging the index at which the first digit is in the string */
-	while (s[index] != '\0')
+	while ((s[index] != '\0') || !((s[index] >= '0') && (s[index] <= '9')))
 	{
-		if ((s[index] >= '0') && (s[index] <= '9'))
-		{
-			break;
-		}
 		index++;
 	}
-
-	/* counting the minus signes before the integer */
-	for (i = 0; i < index + 1; i++)
+	if (index == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < index; i++)
 	{
 		if (s[i] == '-')
 		{
 			minus++;
 		}
 	}
-
-	/* determining the sign of the integer */
 	if (minus % 2 == 0)
 	{
 		int_sign = 1;
@@ -43,21 +37,15 @@ int _atoi(char *s)
 	{
 		int_sign = -1;
 	}
-	
-	/* finding the index of the final digit of the integer */
 	f_index = index;
 	while ((s[f_index] >= '0') && (s[f_index] <= '9'))
 	{
 		f_index++;
 	}
-
-	/* converting the string to integer */
 	the_integer = s[index] - '0';
 	for (i = index + 1; i < f_index; i++)
 	{
 		the_integer = (10 * the_integer) + (s[i] - '0');
 	}
-
 	return (the_integer * int_sign);
-
 }
