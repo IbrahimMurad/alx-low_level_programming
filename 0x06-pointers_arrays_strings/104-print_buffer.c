6be8b72cff2fdc47e40da720212a4e970d287000
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-29 22:18:41 +0000

    more pointers, more fun

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -16,7 +16,7 @@ void print_buffer(char *b, int size)
 	for (i = 0; i < size; i += 10)
 	{
 		printf("%08lx: ", &b[i] - b);
-		for (j = i; j < i + 10; j +=2)
+		for (j = i; j < i + 10; j += 2)
 		{
 			if (j < size)
 			{
