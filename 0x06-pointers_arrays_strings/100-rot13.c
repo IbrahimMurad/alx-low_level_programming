0ae033cfe8b3683b809b73fdcbed297244269d79
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-29 19:25:27 +0000

    more pointers, more fun

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include <ctype.h>
+
+/**
+ * rot13 -  encodes a string using rot13
+ * @s: the string to encode
+ *
+ * Return: a pointer to the encoded string
+ */
+char *rot13(char *s)
+{
+	char *temp = s;
+	char input_arr[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output_arr[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == input_arr[j])
+			{
+				s[i] = output_arr[j];
+				break;
+			}
+		}
+	}
+	return (temp);
+}
