caf0e55a4a5581714ae79792d3353fd713a2819a
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-06-29 15:22:38 +0000

    more pointers, more fun

---
+++ 0x06-pointers_arrays_strings/3-main2.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char s1[] = "Hello";
+	char s2[] = "HelloAA";
+	
+	printf("%d\n", strcmp(s1, s2));
+	printf("%d\n", strcmp(s2, s1));
+	printf("%d\n", strcmp(s1, s1));
+	return (0);
+}
