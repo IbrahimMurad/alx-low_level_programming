48f89fd6557d094d36809362077a5fff681eaf9d
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-07-03 18:05:43 +0000

    Even more pointers, even more fun

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _memcpy - copies memory area
+ * @dest: the pointer to the memory where the copy will be stored
+ * @src: a pointer where the memory area to be copied is
+ * @n: the number of bytes to be copied
+ *
+ * Return: dest (the pointer to the new memory)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
