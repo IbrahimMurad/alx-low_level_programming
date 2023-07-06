d1ce4319dc4fdfbdc4c53715f4952dd01fac2802
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-07-06 21:05:31 +0000

    Recursion is about spinning your head

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+
+/**
+ *  _putchar - writes the character c to stdout
+ *  @c: The character to print
+ *  Return: On success 1.
+ *  On error, -1 is returned, and errno is set appropriately.
+ */
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+
+
+#endif /* MAIN_H */
