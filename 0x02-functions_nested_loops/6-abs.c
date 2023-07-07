e5b800f52c02baaaa51792937296f23f2529948b
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-07-07 14:00:06 +0000

    Functions for functioning

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/**
+ * _abs - computes the absolute value of an integer.
+ * @n: the guven number
+ *
+ * Return: the absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
