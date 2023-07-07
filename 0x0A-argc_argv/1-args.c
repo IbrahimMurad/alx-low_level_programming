bcb5ce49ad5334f91d980fff1a699303046e1ef4
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-07-07 21:13:42 +0000

    my arguments are argv and their number is argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: the number of arguments
+ * @argv: an array of the arguments
+ *
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+}
