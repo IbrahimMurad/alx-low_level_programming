60bc25c4d3f61d0dbca1cfaa532940726c0ee231
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-07-14 01:56:46 +0000

    dynamic memory allocation

--- 0x0C-more_malloc_free/100-main.c
+++ 0x0C-more_malloc_free/100-main.c
@@ -44,13 +44,13 @@ int main(void)
 	int i;
 
 	p = malloc(sizeof(char) * 10);
-	p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
+	p = _realloc(p, sizeof(char) * 10, sizeof(char) * 4);
 	i = 0;
-	while (i < 98)
+	while (i < 4)
 	{
 		p[i++] = 98;
 	}
-	simple_print_buffer(p, 98);
+	simple_print_buffer(p, 4);
 	free(p);
 	return (0);
 }
--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,6 +35,10 @@ void *_realloc(char *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	}
 	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < old_size; i++)
 	{
 		new_ptr[i] = ptr[i];
