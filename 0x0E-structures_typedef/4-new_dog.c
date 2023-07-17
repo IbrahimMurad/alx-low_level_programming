55a25807126a268c42b9d398e6c9fa40d2741369
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-07-17 23:17:31 +0300

    littile puppies

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - creates a new dog.
@@ -14,6 +15,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	char *temp_name;
 	char *temp_owner;
+	unsigned int name_size, owner_size;
+	int i = 0;
 	dog_t *temp;
 
 	if (name == NULL || owner == NULL)
@@ -22,8 +25,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	else
 	{
-		temp_name = name;
-		temp_owner = owner;
+		name_size = strlen(name);
+		owner_size = strlen(owner);
+		temp_name = malloc(name_size + 1);
+		if (temp_name == NULL)
+		{
+			return (NULL);
+		}
+		temp_owner = malloc(owner_size + 1);
+		if (temp_owner == NULL)
+		{
+			free(temp_owner);
+			return (NULL);
+		}
+	}
+	while (name[i] != '\0')
+	{
+		temp_name[i] = name[i];
+		i++;
+	}
+	i = 0;
+	while (owner[i] != '\0')
+	{
+		temp_owner[i] = owner[i];
+		i++;
 	}
 	temp = malloc(sizeof(struct dog));
 	if (temp == NULL)
