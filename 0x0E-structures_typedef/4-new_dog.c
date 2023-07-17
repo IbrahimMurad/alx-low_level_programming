1d4b0ebce2e4c015dab935418353089eab7c8054
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-07-17 22:57:58 +0300

    littile puppies

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,20 +20,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	if (name == NULL)
-	{
-		temp->name = "";
-	}
-	else
 	{
 		temp->name = name;
 	}
 	temp->age = age;
-	if (owner == NULL)
-	{
-		temp->owner = "";
-	}
-	else
 	{
 		temp->owner = owner;
 	}
