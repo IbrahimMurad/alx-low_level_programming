075685fb3cb1356f9e0aef9ac61c3541d591c841
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-07-17 19:02:37 +0300

    littile puppies

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,41 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog.
+ * @name: the new dog's name
+ * @age: the new dog's age
+ * @owner: the new dog's owner's name
+ *
+ * Return: a pointer to the new dog_t type variable
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *temp;
+
+	temp = malloc(sizeof(dog_t));
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+
+	if (name == NULL)
+	{
+		temp->name = "";
+	}
+	else
+	{
+		temp->name = name;
+	}
+	temp->age = age;
+	if (owner == NULL)
+	{
+		temp->owner = "";
+	}
+	else
+	{
+		temp->owner = owner;
+	}
+	return (temp);
+}
