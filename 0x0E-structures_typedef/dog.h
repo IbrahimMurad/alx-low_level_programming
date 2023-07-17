158c0fd3576d015adfe772da350e81f32995ab7b
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-07-17 17:27:15 +0300

    littile puppies

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,17 @@ struct dog
 	char *owner;
 };
 
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: a pointer to the struct dog variable
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner's name
+ *
+ * Return: a pointer to the initialized struct dog variable
+ */
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
