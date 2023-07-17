7ebbecb0a07930464a44aa2843347d066ac577ad
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-07-17 23:38:53 +0300

    littile puppies

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,23 +16,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *temp_name, *temp_owner;
 	dog_t *temp;
 
-	if (name == NULL || owner == NULL)
+	
+	temp_name = malloc(strlen(name) + 1);
+	if (temp_name == NULL)
 	{
 		return (NULL);
 	}
-	else
+	temp_owner = malloc(strlen(owner) + 1);
+	if (temp_owner == NULL)
 	{
-		temp_name = malloc(strlen(name) + 1);
-		if (temp_name == NULL)
-		{
-			return (NULL);
-		}
-		temp_owner = malloc(strlen(owner) + 1);
-		if (temp_owner == NULL)
-		{
-			free(temp_owner);
-			return (NULL);
-		}
+		free(temp_owner);
+		return (NULL);
 	}
 	strcpy(temp_name, name);
 	strcpy(temp_owner, owner);
