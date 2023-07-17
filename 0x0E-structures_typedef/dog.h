20f359de8a02849b95f278f7a2f5bea62e54692c
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-07-17 17:22:49 +0300

    littile puppies

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG
 #define DOG
 
+/**
+ * struct dog - Define a new type struct dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: name of teh owner of the dog
+ *
+ * Description: this data type represents a dog
+ */
+
 struct dog
 {
 	char *name;
