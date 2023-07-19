06fa374c61371a5c4ad1d33345f661a0ec522fd2
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-07-19 13:16:25 +0300

    a function is one step ahead

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
+#include <stddef.h>
+
 #ifndef FUN_PTR
 #define FUN_PTR
 
 void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
