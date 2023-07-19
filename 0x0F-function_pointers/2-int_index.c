a541dcadf1f467840210093048440d23faab09f0
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-07-19 17:39:32 +0300

    a function is one step ahead

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -15,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || cmp == NULL || array == NULL)
 	{
 		return (-1);
 	}
