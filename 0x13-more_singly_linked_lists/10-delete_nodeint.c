e1f1941388d213565fe2ab157ef51b428b347128
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-08-01 12:06:42 +0300

    delete nth node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index index of a listint_t linked list
+ * delete_nodeint_at_index - deletes the index's node of a listint_t list
  * @head: a pointer to a pointer to the first node
  * @index: the index of the node to be deleted
  *
