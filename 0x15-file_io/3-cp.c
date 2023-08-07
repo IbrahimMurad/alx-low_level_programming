ddc52764ef4c4cf887f4c26076d73c297c238a15
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-08-07 22:55:20 +0300

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -106,7 +106,7 @@ int main(int ac, char **av)
 	W = cpyfile(&inFile, &outFile);
 	if (W == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file  %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	if (W == -2)
