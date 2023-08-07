532cef127fc0541c670da900ecd0dcc598217e56
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-08-07 14:55:27 +0300

    create to write, or open to trunicate and allow user to read and write

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file to be created
+ * @text_content: a pointer to a string to be printed to the new file
+ *
+ * Return: 1 on success or -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int newFile;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	newFile = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
+	if (newFile == -1)
+	{
+		return (-1);
+	}
+	dprintf(newFile, "%s", text_content);
+	return (1);
+}
